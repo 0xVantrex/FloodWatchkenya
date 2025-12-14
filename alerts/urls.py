d5c9beeb20a547ae359fdc2c4347f7f3babbe309
0xVantrex <callagansikuche0@gmail.com>
2025-12-14 03:57:51 +0300

    updatre 2.0

--- alerts/urls.py
+++ alerts/urls.py
@@ -3,9 +3,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.list_alerts, name='list_alerts'),          # GET all
-    path('create/', views.create_alert, name='create_alert'),# POST new
-    path('<int:pk>/', views.get_alert, name='get_alert'),    # GET one
-    path('<int:pk>/update/', views.update_alert, name='update_alert'), # PUT
-    path('<int:pk>/delete/', views.delete_alert, name='delete_alert'), # DELETE
+    path('', views.list_alerts, name='list_alerts'),         
+    path('create/', views.create_alert, name='create_alert'),
+    path('<int:pk>/', views.get_alert, name='get_alert'),    
+    path('<int:pk>/update/', views.update_alert, name='update_alert'), 
+    path('<int:pk>/delete/', views.delete_alert, name='delete_alert'), 
 ]
